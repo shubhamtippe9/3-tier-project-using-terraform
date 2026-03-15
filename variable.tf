8d6a34565a81f4639b7e7eb6288e84af91518eab
Prateek Gajbar <pratikgajbar@gmail.com>
2026-03-15 12:39:13 +0530

    Refactor variable descriptions in variable.tf
    Updated descriptions for various variables in variable.tf to improve clarity and consistency.

--- variable.tf
+++ variable.tf
@@ -1,111 +1,125 @@
 variable "region" {
-  default = "ap-south-1"
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
 }
 
+# ---------------- VPC ----------------
+
 variable "mumbai_vpc_cidr" {
-  description = "CIDR of Vpc"
-  type = string
-  default = "10.0.0.0/16"
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
-  description = "Vpc Name"
-  type = string
-  default = "My-vpc"
+  description = "VPC Name"
+  type        = string
+  default     = "my-vpc"
 }
 
+# ---------------- Public Subnet ----------------
+
 variable "public_cidr_block" {
-    description = "Public Subnet CIDR block"
-    type = string
-    default = "10.0.0.0/20"
+  description = "Public Subnet CIDR block"
+  type        = string
+  default     = "10.0.0.0/20"
 }
 
 variable "public_available_zone" {
-    description = "Public subnet availability Zone"
-    type = string
-    default = "ap-south-1a"
+  description = "Public subnet availability zone"
+  type        = string
+  default     = "ap-south-1a"
 }
 
 variable "public_subnet_name" {
-    description = "Public subnet name"
-    type = string
-    default = "public-subnet"
+  description = "Public subnet name"
+  type        = string
+  default     = "public-subnet"
 }
 
+# ---------------- Private Subnet ----------------
+
 variable "private_cidr_block" {
-    description = "Private CIDR Block"
-    type = string
-    default = "10.0.16.0/20"
+  description = "Private Subnet CIDR block"
+  type        = string
+  default     = "10.0.16.0/20"
 }
 
 variable "private_available_zone" {
   description = "Private subnet availability zone"
-  type = string
-  default = "ap-south-1b"
+  type        = string
+  default     = "ap-south-1b"
 }
 
 variable "private_subnet_name" {
-    description = "private subnet name"
-    type = string
-    default = "private-subnet"
+  description = "Private subnet name"
+  type        = string
+  default     = "private-subnet"
 }
+
+# ---------------- Internet Gateway ----------------
 
 variable "igw_name" {
-  description = "Internat Gateway Name"
-  type = string
-  default = "my-igw"
+  description = "Internet Gateway Name"
+  type        = string
+  default     = "my-igw"
 }
 
-variable "nat_name" {
-    description = "NAT Gateway name"
-    type = string
-    default = "my-ngw"
-}
-
-variable "nat_route_table_name" {
-    description = "NAT Gateway route table name"
-    type = string
-    default = "NAT-tb"
-}
+# ---------------- Security Group ----------------
 
 variable "security_group_name" {
- description = "Name of security group" 
- type = string
- default = "My-sg-1"
+  description = "Security Group Name"
+  type        = string
+  default     = "ec2-sg"
 }
 
 variable "description_sg" {
-    description = "Description of Security Group"
-    type = string
-    default = "Allow SSH , HTTP and HTTPS traffic"
+  description = "Security Group Description"
+  type        = string
+  default     = "Allow SSH, HTTP, and Tomcat access for EC2"
 }
 
+# ---------------- Database ----------------
+
+
+variable "db_password" {
+  description = "Password for the RDS MariaDB instance"
+  type        = string
+  sensitive   = true
+}
+
+# ---------------- EC2 Instance ----------------
+
 variable "image_instance" {
-    description = "AMI of ec2 instance"
-    type = string
-    default = "ami-051a31ab2f4d498f5"
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-051a31ab2f4d498f5"
 }
 
 variable "instance_type" {
-  description = "Instance Type"
-  type = string
-  default = "t3.micro"
+  description = "EC2 Instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "instance_key" {
-  description = "Key pair"
-  type = string
-  default = "Ryzen"
+  description = "EC2 Key Pair Name"
+  type        = string
+  default     = "Ryzen"
 }
 
+# ---------------- Instance Names ----------------
+
+
 variable "public_instance_name" {
-    description = "Public Instance name"
-    type = string
-    default = "Proxy-Server"
+  description = "Public EC2 instance name (runs Tomcat)"
+  type        = string
+  default     = "app-server"
 }
 
 variable "private_instance_name" {
-  description = "Private instance name"
-  type = string
-  default = "application-server"
+  description = "Private EC2 instance name (runs DB seed scripts)"
+  type        = string
+  default     = "db-init-server"
 }
